557cd6ea4e04299f19b42615ecb217230805197c
QuocCuong <44748260+QuocCuong0701@users.noreply.github.com>
2025-03-10 10:55:36 +0700

    Update PitchApplication.java

--- src/main/java/com/cuongpq/pitch/PitchApplication.java
+++ src/main/java/com/cuongpq/pitch/PitchApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class PitchApplication {
 
+    //
     public static void main(String[] args) {
         SpringApplication.run(PitchApplication.class, args);
     }
